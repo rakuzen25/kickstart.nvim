34d2e3836c5cf32e98ea630c17375a5d6aed6e8c
Frank <25704248+rakuzen25@users.noreply.github.com>
2024-12-02 12:16:07 -0600

    feat: add monokai pro theme

--- init.lua
+++ init.lua
@@ -834,7 +834,9 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
+  --[=====[
+  {
+    -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
@@ -849,6 +851,20 @@ require('lazy').setup({
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  --]=====]
+
+  {
+    'loctvl842/monokai-pro.nvim',
+    priority = 1000,
+
+    init = function()
+      vim.cmd.colorscheme 'monokai-pro'
+    end,
+
+    config = function()
+      require('monokai-pro').setup()
     end,
   },
 
